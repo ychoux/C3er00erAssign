2c9acc8f4abd818242f7a9fdd4b1c2ed750c0089
junro <junro@192.168.0.134>
2019-10-29 23:34:28 +0800

    idk where to put this file at

---
+++ C3er00erMOB/src/controller/PrintMovieList.java
@@ -0,0 +1,39 @@
+package assignment;
+
+import java.util.List;
+
+public class PrintMovieList {
+
+	public static void printMovieList(){
+		// TODO Auto-generated method stub
+		MovieListCSV file = new MovieListCSV();
+		List<Movie> aList = file.getMovieList();
+		Movie movelist = new Movie();
+		String SplitBy = ";";
+		for(Movie m : aList) {
+			String[] cast = m.getCast().split(SplitBy);
+			String[] genre = m.getGenre().split(SplitBy);
+			System.out.println("Movie: "+m.getMovieTitle());
+			System.out.println("Director: "+m.getDirector());
+			System.out.print("Cast: ");
+			for(String casts: cast)
+			{
+				System.out.print("["+casts+"] ");
+			}
+			System.out.println(" ");
+			System.out.print("Genre: ");
+			for(String genres: genre)
+			{
+				System.out.print("["+genres+"] ");
+			}
+			System.out.println(" ");
+			System.out.println("Status: "+m.getStatus());
+			System.out.println("Sales: "+m.getSales());
+			System.out.println("Rating: "+m.getOverallRating());
+			System.out.println("Synopsis: "+m.getSynopsis());
+			
+		}
+		
+	}
+
+}
