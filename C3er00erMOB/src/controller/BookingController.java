54a6c3503e3581e951518908b2744b56d604a254
kaishern <leek0125@e.ntu.edu.sg>
2019-10-30 22:06:15 +0800

    Customer app in progress

---
+++ C3er00erMOB/src/controller/BookingController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import entity.Slot;
+
+import java.util.List;
+
+public class BookingController {
+    private static BookingController INSTANCE = new BookingController();
+    private List<Slot> result;
+    public static BookingController getInstance() {
+        return INSTANCE;
+    }
+
+    public List<Slot> getResult() {
+        return result;
+    }
+
+    public void setResult(List<Slot> result) {
+        this.result = result;
+    }
+
+    public List<Slot>  viewAvailableSlot(String title) {
+        List<Slot> result;
+        result = SlotManager.getInstance().getMovieSlots("m1s2",title);
+        for (Slot s : result) {
+            System.out.println((result.indexOf(s) + 1) + ". " + s.getCinema().getCineplex_name() + " "
+                    + s.getShowtime().toLocalDate() + " " + s.getShowtime().toLocalTime() + s.getSlotID());
+        }
+        return result;
+    }
+
+    public void planofcineplex(Slot slot){
+        slot.getCinema().getSeating_plan().printSeatingPlan(slot.getBookings());
+    }
+}
--- C3er00erMOB/src/customerApp.java
+++ C3er00erMOB/src/customerApp.java
@@ -1,6 +1,9 @@
+import controller.BookingController;
 import controller.CusMovController;
 import controller.CustomerController;
 import entity.MovieDetail;
+import entity.SeatBookings;
+import entity.Slot;
 
 import java.util.List;
 import java.util.Scanner;
@@ -12,7 +15,7 @@ public class customerApp {
         List<MovieDetail> movieDetailList;
         movieDetailList =  CusMovController.movieCSVRead();
         while(true) {
-            System.out.println("\n\nChoose \n " +
+            System.out.println("\nChoose \n " +
                     "1. View user's detail\n" +
                     " 2. List Movie");
             int choice = sc.nextInt();
@@ -23,6 +26,19 @@ public class customerApp {
                 System.out.println("\nEnter the number of movie to see description: (others to quit)");
                 int ch = sc.nextInt();
                 CusMovController.viewMovieDetails(movieDetailList.get(ch - 1));
+                System.out.println("Enter 1 to show the slots available");
+                int ch1 = sc.nextInt();
+                if(ch1==1){
+                    List<Slot> result;
+                    result = BookingController.getInstance().viewAvailableSlot(movieDetailList.get(ch1 - 1).getTitle());
+                    System.out.println("Choose the slot you are interested in");
+                    int ch2 = sc.nextInt();
+                    Slot slot = result.get(ch2-1);
+                    BookingController.getInstance().planofcineplex(slot);
+                    System.out.println("Enter the seat you want to book");
+                    String seats = sc.next();
+                    SeatBookings sb = new SeatBookings(slot.getCinema().getSeating_plan(),seats);
+                }
             }
             else {
             }
