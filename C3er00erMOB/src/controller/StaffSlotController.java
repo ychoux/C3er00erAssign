796d7258dbc8a68caeb0f4bc7e5a75cbfcd34df6
User <User@10.27.246.94>
2019-11-08 09:39:02 +0800

    staff

--- C3er00erMOB/src/controller/StaffSlotController.java
+++ C3er00erMOB/src/controller/StaffSlotController.java
@@ -1,5 +1,13 @@
 package controller;
 
+import java.util.List;
+
+import entity.Movie;
+
 public class StaffSlotController {
 
+	public void staffAddSlot(List<Movie> movieList) {
+		CusMovController.getInstance().listMovie(movieList);
+	}
+	
 }
