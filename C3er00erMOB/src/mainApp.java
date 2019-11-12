ba8035329921400cc9f19a5100e6624541bf3e70
ychoux <Jia@DESKTOP-LOM2HU6>
2019-11-12 13:13:08 +0800

    Exception handling for main app

--- C3er00erMOB/src/mainApp.java
+++ C3er00erMOB/src/mainApp.java
@@ -1,4 +1,5 @@
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -9,19 +10,26 @@ import java.util.Scanner;
 public class mainApp {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("User:");
+		System.out.println("====================");
 		System.out.println("1. Customer");
 		System.out.println("2. Admin");
-		int choice=sc.nextInt();
-		switch(choice) {
-		case 1:
-			System.out.println("Entering Customer mode...........");
-			customerApp.main(null);
-			break;
-		case 2:
-			System.out.println("Entering Admin mode...........");
-			adminApp.main(null);
-			break;
+		System.out.println("====================");
+		System.out.print("User: ");
+		try {
+			int choice=sc.nextInt();
+			switch(choice) {
+			case 1:
+				System.out.println("Entering Customer mode...........");
+				customerApp.main(null);
+				break;
+			case 2:
+				System.out.println("Entering Admin mode...........");
+				adminApp.main(null);
+				break;
+			}
+		} catch (InputMismatchException e) {
+			System.out.println("Invalid input.");
+			main(null);
 		}
 		sc.close();
 	}
