e8a854614601bf56fe781bda6818d8d81840bdfb
Yuniikon <56990597+Yuniikon@users.noreply.github.com>
2019-11-17 00:03:12 +0800

    Update MovieStatus.java

--- C3er00erMOB/src/entity/MovieStatus.java
+++ C3er00erMOB/src/entity/MovieStatus.java
@@ -1,7 +1,21 @@
 package entity;
 
+/**
+ * This is a enum class for movie status
+ * @author JunRong
+ *
+ */
 public enum MovieStatus {
+	/**
+	 * movie status (Now showing)
+	 */
 	NOW_SHOWING, 
+	/**
+	 * movie status(Up coming)
+	 */
 	UP_COMING,
+	/**
+	 * movie status(end of showing)
+	 */
 	END_OF_SHOWING;
 }
